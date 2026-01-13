a7cf21724cf56af6ab8360e22dff9ab80db4c600
kalharaweragoda <kalharaweragoda@gmail.com>
2026-01-13 11:35:31 +0530

    sign up form add

---
+++ src/main/java/controller/SignUpFormController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SignUpFormController {
+
+    @FXML
+    private JFXButton txtBackToLogin;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtReenterPassword;
+
+    @FXML
+    private JFXButton txtRegister;
+
+    @FXML
+    void btnBackToLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegister(ActionEvent event) {
+
+    }
+
+}
