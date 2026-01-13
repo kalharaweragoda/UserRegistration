997de1ee120e8c1ce4cb4b58b0fdb018debd1cfd
kalharaweragoda <kalharaweragoda@gmail.com>
2026-01-13 11:55:36 +0530

    login_form.fxml add

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,5 +1,32 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+
+    @FXML
+    private JFXButton txtCreateAccount;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private JFXButton txtSignIn;
+
+    @FXML
+    void btnCreateAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignIn(ActionEvent event) {
+
+    }
 
 }
